7ada94c7b004e6f7ecd5e55b6b5943b59b119b56
Tom Dugovic <tom.dugovic@gmail.com>
2024-01-21 14:19:07 -0600

    Add github actions workflow

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MidiMessage.MixProject do
     [
       app: :midi_message,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
