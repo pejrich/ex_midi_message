86764ab192fc9356181f0d5d5a9e8d5e7968b985
Tom Dugovic <tom.dugovic@gmail.com>
2024-02-09 08:11:40 -0600

    Add some known ID Replies to Universal test

--- examples/universal_system_exclusive/test/universal/non_real_time_test.exs
+++ examples/universal_system_exclusive/test/universal/non_real_time_test.exs
@@ -2,7 +2,8 @@ defmodule MidiMessage.SystemExclusive.Universal.NonRealTimeTest do
   use ExUnit.Case, async: true
   use Faker.Random
 
-  alias MidiMessage.SystemExclusive.Universal.NonRealTime
+  alias MidiMessage.SystemExclusive.Universal
+  alias Universal.NonRealTime
 
   describe "decode/1" do
     test "Unknown" do
@@ -17,9 +18,7 @@ defmodule MidiMessage.SystemExclusive.Universal.NonRealTimeTest do
 
       assert NonRealTime.decode(bytes) == message
 
-      assert MidiMessage.decode(bytes,
-               system_exclusive_decoders: [MidiMessage.SystemExclusive.Universal]
-             ) == message
+      assert MidiMessage.decode(bytes, system_exclusive_decoders: [Universal]) == message
 
       assert MidiMessage.encode(message) == bytes
     end
@@ -32,9 +31,7 @@ defmodule MidiMessage.SystemExclusive.Universal.NonRealTimeTest do
 
       assert NonRealTime.decode(bytes) == message
 
-      assert MidiMessage.decode(bytes,
-               system_exclusive_decoders: [MidiMessage.SystemExclusive.Universal]
-             ) == message
+      assert MidiMessage.decode(bytes, system_exclusive_decoders: [Universal]) == message
 
       assert MidiMessage.encode(message) == bytes
     end
@@ -48,10 +45,35 @@ defmodule MidiMessage.SystemExclusive.Universal.NonRealTimeTest do
 
       assert NonRealTime.decode(bytes) == message
 
-      assert MidiMessage.decode(bytes,
-               system_exclusive_decoders: [MidiMessage.SystemExclusive.Universal]
-             ) == message
+      assert MidiMessage.decode(bytes, system_exclusive_decoders: [Universal]) == message
+      assert MidiMessage.encode(message) == bytes
 
+      # Moog Slim Phatty
+      channel = 0x7F
+      id = <<0x04, 0x00, 0x05, 0x00, 0x02, 0x00, 0x00, 0x32, 0x5>>
+
+      bytes = <<0xF0, 0x7E, channel, 0x06, 0x02>> <> id <> <<0xF7>>
+      message = %NonRealTime.IdentityReply{channel: channel, id: id}
+
+      assert NonRealTime.decode(bytes) == message
+
+      assert MidiMessage.decode(bytes, system_exclusive_decoders: [Universal]) == message
+      assert MidiMessage.encode(message) == bytes
+
+      # AKAI MPK Mini Play
+      channel = random_between(0x00, 0x7F)
+
+      id =
+        <<0x47, 0x44, 0x00, 0x19, 0x00, 0x00, 0x03, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+          0x00>>
+
+      bytes = <<0xF0, 0x7E, channel, 0x06, 0x02>> <> id <> <<0xF7>>
+      message = %NonRealTime.IdentityReply{channel: channel, id: id}
+
+      assert NonRealTime.decode(bytes) == message
+
+      assert MidiMessage.decode(bytes, system_exclusive_decoders: [Universal]) == message
       assert MidiMessage.encode(message) == bytes
     end
   end
